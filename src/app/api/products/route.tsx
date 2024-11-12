7e486915bc5f1117ad81d4a12c1729544b7a74e6
Mouhamadou-Soumare <mouhamadou-soumare@hotmail.com>
2024-11-13 00:09:43 +0100

    bug-fixes

--- src/app/api/products/route.tsx
+++ src/app/api/products/route.tsx
@@ -1,4 +1,4 @@
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 import prisma from '../../../../lib/prisma';
 import { Produit } from '../../types';
 
@@ -27,27 +27,57 @@ export async function GET() {
 
 export async function POST(request: Request) {
   try {
+<<<<<<< Updated upstream
     const body: Produit = await request.json();
     const { name, description, prix, categorieId } = body;
+=======
+    const body = await request.json();
 
-    console.log('Received body:', body);
+    // Vérifier si l'appel concerne les ingrédients
+    if (body.ingredients && Array.isArray(body.ingredients) && body.ingredients.length > 0) {
+      const { ingredients } = body;
+
+      const products = await prisma.produit.findMany({
+        where: {
+          name: {
+            in: ingredients,
+          },
+        },
+        include: {
+          image: true,
+        },
+      });
+
+      const transformedProducts = products.map((product) => ({
+        id: product.id,
+        name: product.name,
+        prix: product.prix,
+        imageSrc: product.image?.path || '',
+        slug: product.slug,
+      }));
+
+      return NextResponse.json(transformedProducts, { status: 200 });
+    } 
+
+    // Vérifier si l'appel concerne la création d'un nouveau produit
+    const { name, description, prix, categorieId } = body as Produit;
+>>>>>>> Stashed changes
+
     if (!name || !prix || !description || !categorieId) {
       return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
     }
 
     const slug = name.toLowerCase().replace(/ /g, '-');
-
-    if ('id' in body) {
-      delete body.id;
-    }
     console.log('Creating product with body:', body);
+
     const newProduct = await prisma.produit.create({
       data: {
         name: name,
         description: description,
-        prix: parseFloat(prix.toString()), 
+        prix: parseFloat(prix.toString()),
         slug: slug,
         categorieId: parseInt(categorieId.toString()),
+<<<<<<< Updated upstream
         imageid: null
       },
     });
@@ -57,6 +87,18 @@ export async function POST(request: Request) {
     return NextResponse.json(newProduct, { status: 201 });
   } catch (error) {
     console.error('Error creating product:', error);
+=======
+        imageid: null,
+      },
+      include: {
+        image: true,
+      },
+    });
+
+    return NextResponse.json(newProduct, { status: 201 });
+  } catch (error) {
+    console.error('Error handling POST request:', error);
+>>>>>>> Stashed changes
     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
   }
 }
