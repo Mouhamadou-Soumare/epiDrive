6eab51ea8ff2b9900a623cdfef7ec31d0a906de8
Ibrahima <ibrahima@ohlone.fr>
2024-11-17 15:25:24 +0100

    modified:   next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.watchOptions = {
-          poll: 1000, 
-          aggregateTimeout: 300, 
-        };
-      }
-      return config;
-    },
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      };
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
