82ca891175cde38cfc0abef7e577a018a2933d84
Ibrahima <ibrahima@ohlone.fr>
2024-11-17 17:39:13 +0100

    Update Seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,24 +1,18 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient(); 
+
 import { generateSlug } from './seed/generateSlug.js';
 import { categories } from './seed/categories.js';
 import { createSubcategories } from './seed/subcategories.js';
 import { createUsers } from './seed/users.js';
 import { createRecettes } from './seed/recette.js';
 import { createCommandes } from './seed/commande.js';
-import prisma from '../lib/prisma.js';
-
-
-const prisma = new PrismaClient();
-
-export default prisma; // Export par défaut
-
 
 async function main() {
     try {
-        // Connecter à la base de données Prisma
         await prisma.$connect();
         console.log('Connecté à la base de données.');
 
-        // Appeler les fonctions de seed
         await createUsers();
 
         for (const category of categories) {
@@ -36,10 +30,10 @@ async function main() {
                         description: category.description || null,
                         image: {
                             create: {
-                                path: `/img/category/${slug}.webp` || "default-image-path.jpg", // Utilise l'image spécifiée ou une image par défaut
-                            }
-                        }
-                    }
+                                path: `/img/category/${slug}.webp` || "default-image-path.jpg",
+                            },
+                        },
+                    },
                 });
 
                 console.log(`Catégorie ${parentCategory.name} créée`);
@@ -50,14 +44,12 @@ async function main() {
             }
         }
 
-        // Appeler d'autres fonctions de seed
         await createRecettes();
         await createCommandes();
     } catch (e) {
         console.error('Erreur lors de la création des données :', e);
         process.exit(1);
     } finally {
-        // Déconnecter Prisma
         await prisma.$disconnect();
     }
 }
