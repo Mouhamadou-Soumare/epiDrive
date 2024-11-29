ae3a4e203993f29e0278ef0d6d272a4b308c2337
choeurtis <58913206+choeurtis18@users.noreply.github.com>
2024-11-29 20:29:10 +0100

    Back Office Catégorie

--- src/app/backoffice/categorie/[...slug]/page.tsx
+++ src/app/backoffice/categorie/[...slug]/page.tsx
@@ -1,55 +1,42 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { useParams } from 'next/navigation';
+import { useParams } from "next/navigation";
 import Link from "next/link";
 import Alert from "../components/Alert";
 import DeleteButton from "../components/DeleteButton";
-import { CheckIcon } from '@heroicons/react/20/solid';
-import { Categorie } from "../../../../../types";
+import { CheckIcon } from "@heroicons/react/20/solid";
 import CategorieRow from "../components/CategorieRow";
+import { Categorie } from "types";
+import { useGetCategory, useDeleteCategory } from "@/hooks/categories/useCategories";
 
 export default function CategoryDetails() {
   const params = useParams();
   const slug = params?.slug as string;
-  const [category, setCategory] = useState<Categorie | null>(null);
-  const [loading, setLoading] = useState(true);  
-  const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    async function fetchCategory() {
-      try {
-        const res = await fetch(`/api/categories/${slug}`);
-        const data = await res.json();
-        if (res.ok) setCategory(data);
-        else setError('Error fetching category');
-      } catch (error) {
-        setError('Error fetching category');
-      } finally {
-        setLoading(false);
-      }
-    }
-    fetchCategory();
-  }, [slug]);
+  const { category, loading: categoryLoading, error: categoryError } = useGetCategory(slug) as { category: Categorie | null, loading: any, error: any };
+  const { deleteCategory, loading: deleteLoading, error: deleteError } = useDeleteCategory();
 
   const handleDelete = async () => {
-    try {
-      const res = await fetch(`/api/categories/${category?.slug}`, { method: 'DELETE' });
-      if (res.ok) window.location.assign('/backoffice/categorie');
-      else setError('Error deleting category');
-    } catch (error) {
-      setError('Error deleting category');
+    if (category) {
+      await deleteCategory(category.slug);
+      window.location.href = "/backoffice/categorie";
     }
   };
 
-  if (loading) return <div className="lg:pl-72">Chargement...</div>;
-  if (!category) return <div className="lg:pl-72">Catégorie non trouvée</div>;
+  if (categoryLoading || deleteLoading) return <div className="lg:pl-72">Chargement...</div>;
+  if (categoryError) return <div className="lg:pl-72 text-red-500">Erreur lors de la récupération de la catégorie.</div>;
+  if (!category) return <div className="lg:pl-72">Catégorie non trouvée.</div>;
 
   return (
     <div className="bg-white">
-      {error && <Alert message={error} type="error" />}
+      {(deleteError || categoryError) && (
+        <Alert
+          message={deleteError ? "Erreur lors de la suppression de la catégorie." : "Erreur lors de la récupération de la catégorie."}
+          type="error"
+        />
+      )}
       <div className="mx-auto p-4 sm:p-6 lg:p-8 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8">
-        {/* Category details */}
+        {/* Détails de la catégorie */}
         <div className="lg:max-w-lg lg:self-end">
           <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{category.name}</h1>
           <section aria-labelledby="information-heading" className="mt-4">
@@ -61,10 +48,14 @@ export default function CategoryDetails() {
           </section>
         </div>
 
-        {/* Category image */}
-        <div className="mt-10 lg:col-start-2 lg:row-span-2 lg:mt-0 lg:self-center bg-dark-500">
+        {/* Image de la catégorie */}
+        <div className="mt-10 lg:col-start-2 lg:row-span-2 lg:mt-0 lg:self-center">
           <div className="aspect-h-1 aspect-w-1 overflow-hidden rounded-lg h-96">
-            <img src={`/api/images/${category.imageId}`} alt={category.name} className="h-full w-full object-cover object-center" />
+            <img
+              src={`/api/images/${category.imageId}`}
+              alt={category.name}
+              className="h-full w-full object-cover object-center"
+            />
           </div>
         </div>
 
@@ -72,7 +63,10 @@ export default function CategoryDetails() {
         <div className="mt-10 lg:col-start-1 lg:row-start-2 lg:max-w-lg lg:self-start">
           <section aria-labelledby="options-heading">
             <div className="flex gap-4 flex-col">
-              <Link href={`/backoffice/categorie/update/${category.slug}`} className="flex w-full items-center justify-center bold rounded-md px-8 py-3 text-white bg-orange-300 hover:bg-orange-500 text-black focus:ring-2 focus:ring-indigo-500">
+              <Link
+                href={`/backoffice/categorie/update/${category.slug}`}
+                className="flex w-full items-center justify-center bold rounded-md px-8 py-3 text-white bg-orange-300 hover:bg-orange-500 text-black focus:ring-2 focus:ring-indigo-500"
+              >
                 Modifier la catégorie
               </Link>
               <DeleteButton onDelete={handleDelete} label="Supprimer la catégorie" />
@@ -80,36 +74,37 @@ export default function CategoryDetails() {
           </section>
         </div>
       </div>
-      {/* Subcategories */}
+
+      {/* Sous-catégories */}
       <div className="mx-auto p-4 sm:p-6 lg:p-8">
         <div className="sm:flex sm:items-center">
-            <h1 className="text-base font-semibold text-gray-900">Liste des Sous-categorie</h1>
+          <h1 className="text-base font-semibold text-gray-900">Liste des Sous-catégories</h1>
         </div>
 
         {category.subcategories && category.subcategories.length > 0 ? (
-        <div className="mt-4 flow-root">
-          <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-              <table className="min-w-full divide-y divide-gray-300">
-                <thead>
-                  <tr>
-                    <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">ID</th>
-                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Nom</th>
-                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">slug</th>
-                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Info</th>
-                  </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-200">
-                {category.subcategories.map((subcategory : Categorie) => (
-                  <CategorieRow key={subcategory.id} categorie={subcategory} />
-                ))}
-                </tbody>
-              </table>
+          <div className="mt-4 flow-root">
+            <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+              <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                <table className="min-w-full divide-y divide-gray-300">
+                  <thead>
+                    <tr>
+                      <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">ID</th>
+                      <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Nom</th>
+                      <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Slug</th>
+                      <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Info</th>
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-gray-200">
+                    {category.subcategories.map((subcategory) => (
+                      <CategorieRow key={subcategory.id} categorie={subcategory} />
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
-        </div>
         ) : (
-          <p className="text-center text-gray-500 mt-4">Aucune sous-categorie trouvée.</p>
+          <p className="text-center text-gray-500 mt-4">Aucune sous-catégorie trouvée.</p>
         )}
       </div>
     </div>
--- src/app/backoffice/categorie/update/[...slug]/page.tsx
+++ src/app/backoffice/categorie/update/[...slug]/page.tsx
@@ -1,79 +1,81 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useParams } from 'next/navigation';
 import { Categorie } from "../../../../../../types";
 import FormInputField from "../../components/FormInputField";
 import Alert from "../../components/Alert";
+import { useGetCategory, useGetCategories, useUpdateCategory } from "@/hooks/categories/useCategories";
 
 export default function UpdateCategoryPage() {
-  const { slug } = useParams() as { slug: string }; 
-  const [category, setCategory] = useState<Categorie | null>(null);
-  const [categories, setCategories] = useState<Categorie[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [submitResult, setSubmitResult] = useState<string>('');
+  const { slug } = useParams() as { slug: string };
 
-  useEffect(() => {    
-    async function fetchCategory() {
-      try {
-        const res = await fetch(`/api/categories/${slug}`);
-        const data = await res.json();
-        if (res.ok) setCategory(data);
-        else console.error('Erreur lors de la récupération de la catégorie:', data.error);
-      } catch (error) {
-        console.error('Erreur lors de la récupération de la catégorie:', error);
-      } finally {
-        setLoading(false);
-      }
-    }
-    
-    async function fetchCategories() {
-      const res = await fetch("/api/categories");
-      const data = await res.json();
-      setCategories(data);
-    }
+  const { category, loading: categoryLoading, error: categoryError } = useGetCategory(slug) as { category: Categorie | null, loading: any, error: any };
+  const { categories, loading: categoriesLoading, error: categoriesError } = useGetCategories()
+  const { updateCategory, loading: updateLoading, error: updateError } = useUpdateCategory();
 
-    fetchCategory();
-    fetchCategories();
-  }, [slug]);
+  const [submitResult, setSubmitResult] = useState<string | null>(null);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
+  ) => {
     const { name, value } = e.target;
-    setCategory((prev) => prev ? { ...prev, [name]: value } : null);
+    if (category) {
+      const updatedCategory = { ...category, [name]: name === "imageId" || name === "parentId" ? Number(value) : value };
+      Object.assign(category, updatedCategory);
+    }
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     if (category) {
-      try {
-        const res = await fetch(`/api/categories/${category.slug}`, {
-          method: "PATCH",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(category),
-        });
-        if (res.ok) setSubmitResult('200');
-        else setSubmitResult('error');
-      } catch (error) {
-        setSubmitResult('error');
-        console.error('Erreur lors de la mise à jour de la catégorie:', error);
-      }
+      const success = await updateCategory(slug, category);
+      setSubmitResult(success ? 'success' : 'error');
     }
   };
 
-  if (loading) return <div className="lg:pl-72">Chargement...</div>;
-  if (!category) return <div className="lg:pl-72">Catégorie non trouvée</div>;
+  if (categoryLoading || categoriesLoading || updateLoading) return <div className="lg:pl-72">Chargement...</div>;
+  if (categoryError || categoriesError) return <div className="lg:pl-72 text-red-500">Erreur lors du chargement des données.</div>;
+  if (!category) return <div className="lg:pl-72">Catégorie non trouvée.</div>;
 
   return (
     <>
-      {submitResult && <Alert message="Category updated successfully" type="success" />}
-      <h1 className='text-3xl font-extrabold leading-tight text-gray-900'>Update Category</h1>
+      {submitResult && (
+        <Alert
+          message={submitResult === 'success' ? 'Catégorie mise à jour avec succès.' : 'Une erreur est survenue.'}
+          type={submitResult === 'success' ? 'success' : 'error'}
+        />
+      )}
+      <h1 className="text-3xl font-extrabold leading-tight text-gray-900">Modifier la catégorie</h1>
       <form onSubmit={handleSubmit} className="my-6">
-        <FormInputField id="name" name="name" value={category.name} label="Name" onChange={handleInputChange} />
-        <FormInputField id="description" name="description" value={category.description} label="Description" type="textarea" onChange={handleInputChange} />
-        <FormInputField id="imageId" name="imageId" value={category.imageId} label="Image ID" type="number" onChange={handleInputChange} />
+        <FormInputField
+          id="name"
+          name="name"
+          value={category.name}
+          label="Nom"
+          onChange={handleInputChange}
+        />
+        <FormInputField
+          id="description"
+          name="description"
+          value={category.description}
+          label="Description"
+          type="textarea"
+          onChange={handleInputChange}
+        />
+        <FormInputField
+          id="imageId"
+          name="imageId"
+          value={category.imageId || ''}
+          label="ID de l'image"
+          type="number"
+          onChange={handleInputChange}
+        />
 
         <div className="mb-5">
-          <label htmlFor="parentId" className="block mb-2 text-sm font-medium text-gray-900">Parent Category</label>
+          <label htmlFor="parentId" className="block mb-2 text-sm font-medium text-gray-900">
+            Catégorie parente
+          </label>
           <select
             id="parentId"
             name="parentId"
@@ -81,15 +83,21 @@ export default function UpdateCategoryPage() {
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             onChange={handleInputChange}
           >
-            <option value="">Select a parent category</option>
-            {categories.map((cat) => (
-              <option key={cat.id} value={cat.id}>{cat.name}</option>
+            <option value="">Sélectionner une catégorie parente</option>
+            {categories.map((cat: Categorie) => (
+              <option key={cat.id} value={cat.id}>
+                {cat.name}
+              </option>
             ))}
           </select>
         </div>
-        
-        <button type="submit" 
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Update Category</button>
+
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+        >
+          Mettre à jour la catégorie
+        </button>
       </form>
     </>
   );
