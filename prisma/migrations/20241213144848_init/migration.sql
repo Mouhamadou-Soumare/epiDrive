d0e8020221010db867eb5aa982190b9c9315a935
Ibrahima <ibrahima.diallo@eemi.com>
2024-12-17 16:55:28 +0100

    Suppression des anciennes migrations Prisma obsolètes et nettoyage des fichiers de migration

--- prisma/migrations/20241213144848_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[fk_commande,fk_produit]` on the table `QuantiteCommande` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `CommandeProduit_unique` ON `QuantiteCommande`(`fk_commande`, `fk_produit`);
