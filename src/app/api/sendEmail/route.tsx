7f952727517a0f19a166ec7fe789605ff1ee5f37
choeurtis <58913206+choeurtis18@users.noreply.github.com>
2024-12-17 19:12:38 +0100

    remove console.log

--- src/app/api/sendEmail/route.tsx
+++ src/app/api/sendEmail/route.tsx
@@ -3,8 +3,6 @@
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
-console.log('process.env.RESEND_API_KEY', process.env.RESEND_API_KEY);
-console.log("testttttt");
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
