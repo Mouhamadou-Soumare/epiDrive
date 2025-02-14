a1b58275b0d06edac49b620d32ebbe19a53e70da
Mouhamadou-Soumare <mouhamadou-soumare@hotmail.com>
2025-02-14 02:50:39 +0100

    Update HeroSectionHomepage.tsx

--- src/components/HeroSectionHomepage.tsx
+++ src/components/HeroSectionHomepage.tsx
@@ -39,7 +39,7 @@ export default function HeroSectionHomepage() {
               Découvrez des produits locaux, simplifiez vos achats, gagnez du temps.</p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
-                  href="/"
+                  href="/category"
                   className="rounded-md button-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Commencez dès maintenant
